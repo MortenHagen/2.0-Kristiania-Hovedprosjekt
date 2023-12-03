8c988cb4cd3764dee8603a48165c8fd163fd39f5
Morten Hagen <144051167+MortenHagen@users.noreply.github.com>
2023-12-03 20:52:10 +0100

    finished create-account, startet my-site.html

--- js/account__create.js
+++ js/account__create.js
@@ -1,27 +1,32 @@
-	
-	
-	
-	function saveToLocalStorage() {
-		  // Get the user input
-		  const namePrivate = document.querySelector('#namePrivate').value;
-		  const emailPrivate = document.querySelector('#emailPrivate').value;
-		  const phoneNumberPrivatePrivate = document.querySelector('#phoneNumberPrivatePrivate').value;
-		  const birthdatePrivate = document.querySelector('#birthdatePrivate').value;
-		  const genderPrivate = document.querySelector('#genderPrivate').value;
+function privateToLocalStorage() {
 
-		  // Check if the input is not empty
-		  if (userInput.trim() !== '') {
-				// Store the input in local storage
-				localStorage.setItem('namePrivate', namePrivate);
-				localStorage.setItem('emailPrivate', emailPrivate);
-				localStorage.setItem('phoneNumberPrivatePrivate', phoneNumberPrivatePrivate);
-				localStorage.setItem('birthdatePrivate', birthdatePrivate);
-				localStorage.setItem('genderPrivate', genderPrivate);
+	const namePrivate = document.querySelector('#namePrivate').value;
+	const emailPrivate = document.querySelector('#emailPrivate').value;
+	const phoneNumberPrivatePrivate = document.querySelector('#phoneNumberPrivate').value;
+	const birthdatePrivate = document.querySelector('#birthdatePrivate').value;
+	const genderPrivate = document.querySelector('#genderPrivate').value;
+ 
+	if (namePrivate.trim() !== '') {
+		localStorage.setItem('namePrivateSave', namePrivate);
+ 
+	const confirmationMessage = 'User input saved to local storage: ' + namePrivate + '\nClick OK to redirect.';
+	const userConfirmed = confirm(confirmationMessage);
 
-				// Optionally, you can also display a confirmation or perform other actions
-				alert('User input saved to local storage: ' + namePrivate);
-		  } else {
-				// Display an error or handle the case when the input is empty
-				alert('Please enter a value before saving.');
-		  }
-	 }
+	if (userConfirmed) {
+		window.location.href = 'my-site.html';
+	}
+	} else {
+		alert('Please enter a value before saving.');
+	};
+};
+
+const submitButton = document.querySelector('.create-account__submit-button');
+submitButton.addEventListener('click', privateToLocalStorage);
+
+const checkDataStored = document.querySelector('.create-account__submit-button2');
+
+function displatStoredData() {
+	console.log(localStorage.getItem('namePrivateSave'));
+}
+
+checkDataStored.addEventListener('click', displatStoredData);
